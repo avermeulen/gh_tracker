34dcb4c01153b4b44b083b80f3cf66bd130e48a5
avermeulen <andre.vermeulen@gmail.com>
2015-06-09 21:39:04 +0200

    added database scripts coders & events

--- db.sql
+++ db.sql
@@ -1,3 +1,12 @@
+create table coders(
+	id int auto_increment not null,
+	firstName char(100),
+	lastName char(100),
+	email char(100),
+	username char(100) unique,
+	primary key(id)
+);
+
 create table events (
 	id varchar(30) not null,
 	type char(30),
@@ -6,5 +15,6 @@ create table events (
 	repositoryUrl char(100),
 	repositoryName  char(60),
 	coder_id int,
-	--CONSTRAINT FOREIGN KEY (coder_id) REFERENCES coders(id)
-	PRIMARY KEY (id));
+	constraint foreign key (coder_id) references coders(id),
+	primary key (id)
+);
