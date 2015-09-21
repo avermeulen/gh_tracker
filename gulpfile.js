de989c70a083e5685c7a7cef99cd3f74bafc48f5
avermeulen <andre.vermeulen@gmail.com>
2015-09-21 22:34:09 +0200

    auto generate react file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
- 
+var watch = require('gulp-watch');
+
 gulp.task('default', function () {
     return gulp.src('./react/app.react.jsx')
+        .pipe(watch('./react/app.react.jsx'))
         .pipe(react())
         .pipe(gulp.dest('public'));
 });
